56328b0219b050126dc4c2836dca1b135023a5b3
rahuang <rahuang@andrew.cmu.edu>
2017-05-24 13:46:50 -0400

    Established standards for using scopes or managers and querysets

--- books/models.py
+++ books/models.py
@@ -1,26 +1,58 @@
 from django.db import models
+from django.utils import timezone
+
+# Models
 
 class Author(models.Model):
+    # Author fields
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+    # Scopes/Manager
+    class QuerySet(models.QuerySet):
+        def alphabetical(self):
+            return self.order_by("last_name", "first_name")
+
+    objects = QuerySet.as_manager()
 
     def __str__(self):
         return self.first_name + " " + self.last_name
 
 
 class Publisher(models.Model):
+    # Publisher fields
     name = models.CharField(max_length=255)
+
+    # Scopes/Manager
+    class QuerySet(models.QuerySet):
+        def alphabetical(self):
+            return self.order_by("name")
+
+    objects = QuerySet.as_manager()
 
     def __str__(self):
         return self.name
 
 
 class Book(models.Model):
+    # Book fields
     title = models.CharField(max_length=255)
-    year_published = models.IntegerField(default=0)
+    year_published = models.IntegerField(default=timezone.now().year)
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     authors = models.ManyToManyField(Author)
 
+    # Scopes/Manager
+    class QuerySet(models.QuerySet):
+        def alphabetical(self):
+            return self.order_by("title")
+
+        def last_decade(self):
+            decade_ago = timezone.now().year - 10
+            return self.filter(year_published__gt=decade_ago)
+
+    objects = QuerySet.as_manager()
+
     def __str__(self):
         return self.title
+
 
