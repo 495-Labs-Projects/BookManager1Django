abc0c730837d4a8bb1673a1ab3b41f6cf5209867
rahuang <rahuang@andrew.cmu.edu>
2017-05-24 01:02:16 -0400

    Added models to the books app

--- books/models.py
+++ books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    year_published = models.IntegerField(default=0)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.title
+
