2cc18e7a211d7b31df39b314137354f79ad052e5
rahuang <rahuang@andrew.cmu.edu>
2017-05-25 23:18:18 -0400

    Added author view tests

--- books/tests/test_views.py
+++ books/tests/test_views.py
@@ -74,6 +74,73 @@ class BookViewTests(FactoryTestCase):
         self.assertRedirects(response, reverse('books:book_list'))
 
 
+class AuthorViewTests(FactoryTestCase):
+
+    def setUp(self):
+        self.factories.populate_books()
+
+    def test_list_view_with_no_authors(self):
+        Author.objects.all().delete()
+
+        response = self.client.get(reverse('books:author_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No authors are available.")
+        self.assertQuerysetEqual(response.context['object_list'], [])
+
+    def test_list_view_with_authors(self):
+        response = self.client.get(reverse('books:author_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(list(response.context['object_list']), 
+            [repr(self.factories.a4), repr(self.factories.a3), repr(self.factories.a2), repr(self.factories.a1)])
+
+    def test_new_author_view(self):
+        response = self.client.get(reverse('books:author_new'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context['form'], AuthorForm)
+        self.assertContains(response, "Create Author")
+
+    def test_create_author_view(self):
+        num_authors = Author.objects.count()
+        response = self.client.post(reverse('books:author_new'),
+            {'first_name': 'Test', 'last_name': 'Author'}) 
+        self.assertEqual(Author.objects.count(), num_authors + 1)
+        self.assertRedirects(response, reverse('books:author_detail', args=(num_authors+1,)))
+
+    def test_create_bad_author_view(self):
+        num_authors = Author.objects.count()
+        response = self.client.post(reverse('books:author_new'),
+            {'first_name': '', 'last_name': 'Author'}) 
+        self.assertEqual(Author.objects.count(), num_authors)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context['form'], AuthorForm)
+
+    def test_edit_author_view(self):
+        response = self.client.get(reverse('books:author_edit', args=(self.factories.a1.id,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context['form'], AuthorForm)
+        self.assertContains(response, "Update Author")
+
+    def test_update_author_view(self):
+        response = self.client.post(reverse('books:author_edit', args=(self.factories.a1.id,)),
+            {'first_name': 'Test', 'last_name': 'Author'})
+        self.factories.a1.refresh_from_db()
+        self.assertEqual(self.factories.a1.first_name, 'Test')
+        self.assertRedirects(response, reverse('books:author_detail', args=(self.factories.a1.id,)))
+
+    def test_update_bad_author_view(self):
+        response = self.client.post(reverse('books:author_edit', args=(self.factories.a1.id,)),
+            {'first_name': '', 'last_name': 'Author'})
+        self.factories.a1.refresh_from_db()
+        self.assertEqual(self.factories.a1.first_name, 'John')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context['form'], AuthorForm)
+
+    def test_delete_book_view(self):
+        num_authors = Author.objects.count()
+        import pdb; pdb.set_trace()
+        response = self.client.post(reverse('books:author_delete', args=(self.factories.a3.id,)))
+        self.assertEqual(Author.objects.count(), num_authors - 1)
+        self.assertRedirects(response, reverse('books:author_list'))
 
 
         # num_publishers = Publisher.objects.count()
